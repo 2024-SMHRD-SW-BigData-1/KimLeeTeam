84496851beeee44af6e9d1010fe5946bae6cf77d
이선우 <woolee0822@naver.com>
2024-04-15 11:39:53 +0900

    branch 추가

--- src/Ex01.java
+++ src/Ex01.java
@@ -2,14 +2,20 @@
 public class Ex01 {
 
 	public static void main(String[] args) {
-		
-	
 		System.out.println("팀장 첫번째 커밋");
 		System.out.println("팀원 두번쨰 커밋");
 		System.out.println("팀원 세번쨰 커밋");
 		System.out.println("팀원 네번째 커밋");
 		System.out.println("팀원 마지막 커밋");
 
+		System.out.println("Teset123");
+		
+		
+		
+		System.out.println("나만의 작업공간(branch)에 커밋하기!!");
+			
+	
+		
 	}
 
 }
