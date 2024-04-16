db0bacae2d7e99a5a13d9cb99a53d65b8956a903
KMS <rwd0427@gmail.com>
2024-04-16 15:05:47 +0900

    민석

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,16 +1,11 @@
 package View;
 
 public class Main {
-<<<<<<< HEAD
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
-	}
-
-=======
-	public static void main(String[] args) {
-		System.out.println("도전!");
-	}
->>>>>>> 0456d737dcd1179d17502c70085eff204fc511b7
+		
+   }
 }
+	
