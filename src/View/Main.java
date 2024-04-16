b27662351cb87bb82b441c1cccf59378be47a5bd
이선우 <woolee0822@naver.com>
2024-04-16 15:27:01 +0900

    선우

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,14 +1,45 @@
 package View;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("명대사 듣고 영화,드라마 제목 맞추기");
+		while(true) {
+			System.out.print("[1]회원가입 [2]로그인 [3]아이디찾기 [4]비밀번호찾기 [5]회원탈퇴 [6]게임나가기");
+			int select = sc.nextInt();
+			if(select == 1) {
+				//회원가입
+			}else if(select == 2) {
+				//로그인
+			}else if(select == 3) {
+				//아이디찾기
+			}else if(select == 4) {
+				//비밀번호찾기
+			}else if(select == 5) {
+				//회원탈퇴
+			}else if(select == 6) {
+				//게임나가기
+			}
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	
-
-
-		System.out.println("도전!");
-		System.out.println("ex01 삭제");
 	}
 
 }
